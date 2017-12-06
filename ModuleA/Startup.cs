477e0d66332054e26c8ce4d6c753116886b26fd4
Matthew Capewell <matthew.capewell@ymx.yokogawa.com>
2017-12-06 11:45:02 +0000

    Add project files.

---
+++ ModuleA/Startup.cs
@@ -0,0 +1,22 @@
+﻿using OrchardCore.Modules;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Builder;
+
+namespace ModuleA
+{
+    public class Startup : StartupBase
+    {
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaRoute
+            (
+                name: "Home",
+                areaName: "ModuleA",
+                template: "",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
---
+++ ModuleB/ViewComponents/TestViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuleB.ViewComponents
+{
+    public class TestViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
